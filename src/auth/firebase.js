752aa14077728a5f126c4ea0bb7aae315a8bc2b1
barisbkk <brs2464@gmail.com>
2024-03-17 14:57:01 +0300

    .env created and firebase.js fixed

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -7,16 +7,16 @@ import { getAuth } from "firebase/auth";
 //! firebase console settings bölümünden firebaseconfig ayarlarını al
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbKGxnT4Xp-FY_m3Ax5o5Yg0qAJK_sPh8",
-  authDomain: "movie-app-f1165.firebaseapp.com",
-  projectId: "movie-app-f1165",
-  storageBucket: "movie-app-f1165.appspot.com",
-  messagingSenderId: "292941928619",
-  appId: "1:292941928619:web:65f9c1cc0a2bd4a1927fee",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+export const auth = getAuth(app);
