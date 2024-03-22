bab6881bc5219e695e6281c85ffaf5b650d83640
barisbkk <brs2464@gmail.com>
2024-03-22 13:02:33 +0300

    ToastNotify added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AppRouter from "./router/AppRouter";
 import AuthContextProvider from "./context/AuthContext";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <AuthContextProvider>
       <AppRouter />
+      <ToastContainer />
     </AuthContextProvider>
   );
 };
